67bad09ec5efbaa29d2d6426dc5797b6e4864550
crafn <tuukka.pensala@gmail.com>
2015-03-21 18:42:59 +0200

    Draw all selected resources in editor regardless of current mode

--- code/source/editor/editor.c
+++ code/source/editor/editor.c
@@ -385,8 +385,7 @@ void gui_mesh_overlay(U32 *model_h, bool *is_edit_mode)
 			toggle_bool(&m->vertices[closest_i].selected);
 	}
 
-	// Draw
-
+	// Draw vertices
 	if (*is_edit_mode) {
 		for (U32 i= 0; i < m->mesh_v_count; ++i) {
 			TriMeshVertex *v= &m->vertices[i];
@@ -404,20 +403,6 @@ void gui_mesh_overlay(U32 *model_h, bool *is_edit_mode)
 				ddraw_poly((Color) {0.0, 0.0, 0.0, 0.8}, poly, 4);
 		}
 	}
-
-	Color fill_color= {0.6, 0.6, 0.8, 0.4};
-	if (!*is_edit_mode)
-		fill_color= (Color) {1.0, 0.8, 0.5, 0.6};
-
-	V3d poly[3];
-	for (U32 i= 0; i < m->mesh_i_count; ++i) {
-		U32 v_i= m->indices[i];
-		V3d p= vertex_world_pos(m, v_i);
-		poly[i%3]= p;
-
-		if (i % 3 == 2)
-			ddraw_poly(fill_color, poly, 3);
-	}
 }
 
 F64 sign(F64 a)
@@ -430,7 +415,6 @@ void gui_armature_overlay(U32 *comp_h, bool *is_edit_mode)
 	UiContext *ctx= g_env.uicontext;
 	V3d cur_wp= v2d_to_v3d(screen_to_world_point(ctx->dev.cursor_pos));
 	V3d prev_wp= v2d_to_v3d(screen_to_world_point(ctx->dev.prev_cursor_pos));
-	F64 v_size= editor_vertex_size();
 
 	const char *box_label= "armature_overlay_box";
 	EditorBoxState state=
@@ -491,9 +475,7 @@ void gui_armature_overlay(U32 *comp_h, bool *is_edit_mode)
 		}
 
 		a->res.needs_saving= true;
-		calc_global_pose(global_pose, entity);
 	}
-
 
 	if (*is_edit_mode && state.pressed) {
 		// Control joint selection
@@ -518,38 +500,6 @@ void gui_armature_overlay(U32 *comp_h, bool *is_edit_mode)
 		if (closest_dist < 2.0) {
 			ensure(closest_i != NULL_HANDLE);
 			toggle_bool(&a->joints[closest_i].selected);
-		}
-	}
-
-	// Draw
-
-	Color default_color= {0.6, 0.6, 0.8, 0.8};
-	Color selected_color= {1.0, 0.8, 0.5, 0.7};
-	Color line_color= {0.0, 0.0, 0.0, 1.0};
-	if (!*is_edit_mode)
-		line_color= selected_color;
-
-	for (U32 i= 0; i < a->joint_count; ++i) {
-		V3d p= global_pose[i].pos;
-
-		const U32 v_count= 15;
-		V3d v[v_count];
-		for (U32 i= 0; i < v_count; ++i) {
-			F64 a= i*3.141*2.0/v_count;
-			v[i].x= p.x + cos(a)*v_size*3;
-			v[i].y= p.y + sin(a)*v_size*3;
-			v[i].z= 0.0;
-		}
-
-		Color c= default_color;
-		if (a->joints[i].selected || !*is_edit_mode)
-			c= selected_color;
-		ddraw_poly(c, v, v_count);
-
-		if (a->joints[i].super_id != NULL_JOINT_ID) {
-			ddraw_line(	line_color,
-						p,
-						global_pose[a->joints[i].super_id].pos);
 		}
 	}
 }
@@ -609,5 +559,72 @@ void upd_editor()
 		gui_armature_overlay(&e->cur_comp_h, &e->is_edit_mode);
 	} break;
 	default: fail("Unhandled editor state: %i", e->state);
+	}
+
+	// Draw selected things on world
+	const Color inactive_color= {0.5, 0.5, 0.5, 0.5};
+
+	if (e->cur_model_h != NULL_HANDLE) {
+		ModelEntity	*m= get_modelentity(e->cur_model_h);
+		Color fill_color= {0.6, 0.6, 0.8, 0.4};
+		if (!e->is_edit_mode)
+			fill_color= (Color) {1.0, 0.8, 0.5, 0.6};
+
+		Color poly_color= fill_color;
+		if (e->state != EditorState_mesh)
+			poly_color= inactive_color;
+
+		V3d poly[3];
+		for (U32 i= 0; i < m->mesh_i_count; ++i) {
+			U32 v_i= m->indices[i];
+			V3d p= vertex_world_pos(m, v_i);
+			poly[i%3]= p;
+
+			if (i % 3 == 2)
+				ddraw_poly(poly_color, poly, 3);
+		}
+	}
+
+	if (e->cur_comp_h != NULL_HANDLE){
+		CompEntity *entity= get_compentity(e->cur_comp_h);
+		Armature *a= entity->armature;
+		T3d global_pose[MAX_ARMATURE_JOINT_COUNT];
+		calc_global_pose(global_pose, entity);
+
+		Color default_color= {0.6, 0.6, 0.8, 0.8};
+		Color selected_color= {1.0, 0.8, 0.5, 0.7};
+		Color line_color= {0.0, 0.0, 0.0, 1.0};
+		if (!e->is_edit_mode)
+			line_color= selected_color;
+		if (e->state != EditorState_armature) {
+			default_color= inactive_color;
+			selected_color= inactive_color;
+			line_color= inactive_color;
+		}
+
+		F64 v_size= editor_vertex_size();
+		for (U32 i= 0; i < a->joint_count; ++i) {
+			V3d p= global_pose[i].pos;
+
+			const U32 v_count= 15;
+			V3d v[v_count];
+			for (U32 i= 0; i < v_count; ++i) {
+				F64 a= i*3.141*2.0/v_count;
+				v[i].x= p.x + cos(a)*v_size*3;
+				v[i].y= p.y + sin(a)*v_size*3;
+				v[i].z= 0.0;
+			}
+
+			Color c= default_color;
+			if (a->joints[i].selected || !e->is_edit_mode)
+				c= selected_color;
+			ddraw_poly(c, v, v_count);
+
+			if (a->joints[i].super_id != NULL_JOINT_ID) {
+				ddraw_line(	line_color,
+							p,
+							global_pose[a->joints[i].super_id].pos);
+			}
+		}
 	}
 }
