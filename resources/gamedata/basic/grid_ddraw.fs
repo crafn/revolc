d28c0fb4400671e1a86e72f53cba2f064f22d6dc
Tuukka Pensala <tuukka.pensala@frozenbyte.com>
2015-03-26 23:19:31 +0200

    Removed resources from repo

--- resources/gamedata/basic/grid_ddraw.fs
+++
@@ -1,9 +0,0 @@
-#version 150 core
-uniform sampler2D u_tex_color;
-in vec3 v_uv;
-
-out vec4 f_color;
-void main()
-{
-	f_color= texture(u_tex_color, v_uv.xy);
-}
--- resources/gamedata/basic/missing.fs
+++
@@ -1,8 +0,0 @@
-#version 150 core
-uniform sampler2D u_tex_color;
-in vec2 v_uv;
-out vec4 f_color;
-void main()
-{
-	gl_FragColor= texture2D(u_tex_color, v_uv);
-}
