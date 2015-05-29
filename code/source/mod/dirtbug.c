e0799bb6cff160a830a0512dd4b16b1735c33a48
Tuukka Pensala <tuukka.pensala@frozenbyte.com>
2015-05-29 16:26:03 +0300

    Started working on dirtbug

---
+++ code/source/mod/dirtbug.c
@@ -0,0 +1,32 @@
+#include "animation/clip.h"
+#include "animation/joint.h"
+#include "audio/audiosystem.h"
+#include "build.h"
+#include "core/debug_print.h"
+#include "core/ensure.h"
+#include "core/random.h"
+#include "core/vector.h"
+#include "core/scalar.h"
+#include "global/env.h"
+#include "game/world.h"
+#include "physics/chipmunk_util.h"
+#include "physics/rigidbody.h"
+#include "physics/physworld.h"
+#include "physics/query.h"
+#include "platform/device.h"
+#include "resources/resblob.h"
+#include "visual/renderer.h" // screen_to_world_point, camera
+
+typedef struct DirtBug {
+	RigidBody *body;
+} DirtBug;
+
+
+MOD_API void upd_dirtbug(DirtBug *bug, DirtBug *bug_end)
+{
+	//F64 dt= g_env.world->dt;
+
+	for (; bug != bug_end; ++bug) {
+		debug_print("DirtBug %f", bug->body->tf.pos.x);
+	}
+}
