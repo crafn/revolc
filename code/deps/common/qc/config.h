cdd6958e9b56364b2eef20982b469000b2a8d9c1
Tuukka Pensala <tuukka.pensala@gmail.com>
2016-03-24 17:47:58 +0200

    Added qc/config.h

---
+++ code/deps/common/qc/config.h
@@ -0,0 +1,18 @@
+#ifndef QC_CONFIG_H
+#define QC_CONFIG_H
+
+/* Default library settings */
+
+#ifndef QC_MALLOC
+#	define QC_MALLOC malloc
+#endif
+
+#ifndef QC_FREE
+#	define QC_FREE free
+#endif
+
+#ifndef QC_API
+#	define QC_API
+#endif
+
+#endif
